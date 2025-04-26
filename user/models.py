8afe0df66c169a6d7b57cb8cf683347603f5c4ab
Duc An <nguyenducan30122002@gmail.com>
2025-04-26 10:33:08 +0700

    add git ignore

--- user/models.py
+++ user/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from base.models import BaseModel
+
 
 # Create your models here
-class User(AbstractUser):
+class User(AbstractUser, BaseModel):
+    name=models.CharField(max_length=100)
     pass
